9f01896a66ed3cca7a2a61375a2c32c3b71b29c7
iagoricogarcia-stack <iagoricogarcia@gmail.com>
2026-09-22 18:20:35 +0200

    Refactor variables and add nodes and edges
    Added new variables for infrastructure and nodes, and updated edges.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import metodos as ms
 
 edificio = [500, 50, 90]
-piso = [100, 10, 33]
+infraestructura = [100, 10, 33]
 parking = [180, 30, 15]
 chalet = [300, 30, 60]
 parcela = [300, 100, 10]
 farmacia = [250, 120, 20]
 almacen = [100, 30, 40]
 supermercado = [800, 200, 100]
+
+nodos= ["edificio","infraestructura","parking","chalet","parcela","almacen","parking","farmacia","supermercado"]
+aristas = [["edificio","infraestructura"],["chalet","parcela"],["parcela","parking"],["farmacia","almacen"],["supermercado","almacen"]]
