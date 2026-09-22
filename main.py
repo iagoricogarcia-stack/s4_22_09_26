97a4ad04db843fbecc870b0c70b1d6b0db293fd6
CarlosMMTT <carlostictra@gmail.com>
2026-09-22 17:41:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,7 @@ def suma_de_listas(lista1, lista2):
         sum_resultado = lista1[i] + lista2[i]
         linea.append(sum_resultado)
     return linea
-list1 = [1, 2, 3]
-list2 = [4, 5, 6]
-
-print(suma_de_listas(list1, list2))
-
-matrix1 = [[2, 3, 4], [5, 6, 7]]
-matrix2 = [[1, 2, 1], [2, 1, 2]]
-
-
-
+    
 def suma_de_matrices(matriz1, matriz2):
     columna = []
     for i in range(len(matriz1)):
@@ -21,3 +12,15 @@ def suma_de_matrices(matriz1, matriz2):
         columna.append(sum_resultado)
     return columna
 print(suma_de_matrices(matrix1, matrix2))
+
+def ordenamiento_por_insercion(lista): 
+    lista_ordenada = lista[:] 
+    for i in range(1, len(lista_ordenada)): 
+        clave = lista_ordenada[i] 
+        j = i - 1 
+        while j >= 0 and lista_ordenada[j] > clave: 
+            lista_ordenada[j + 1] = lista_ordenada[j] 
+            j -= 1 
+    lista_ordenada[j + 1] = clave 
+    print(lista_ordenada)
+    return lista_ordenada
