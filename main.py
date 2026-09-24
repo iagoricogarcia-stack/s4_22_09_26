fc96456fc1902ea60bb8e07e5e2bea81eae162cd
iagoricogarcia-stack <iagoricogarcia@gmail.com>
2026-09-24 20:15:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,52 @@ supermercado = [800, 200, 100]
 
 nodos= ["edificio","infraestructura","parking","chalet","parcela","almacen","parking","farmacia","supermercado"]
 aristas = [["edificio","infraestructura"],["chalet","parcela"],["parcela","parking"],["farmacia","almacen"],["supermercado","almacen"]]
+
+listadeificios = []
+
+dependencias = {
+    "edificio": ["infraestructura"],
+    "infraestructura": ["parking"],
+    "parking": [""],
+    "chalet": ["parcela"],
+    "parcela": ["parking"],
+    "almacen": [""],
+    "farmacia": ["almacen"],
+    "supermercado": ["almacen"]
+}
+
+
+def construir(lista1):
+     
+    # Comprobamos si poseemos de los materiales y el dinero 
+    a = int(input("¿Qué materiales tienes?"))
+    b = int(input("¿Qué dinero tienes?"))
+    
+    # Preguntamos cuantos obreros usará
+    obreros = int(input("¿Cuántos obreros tienes?"))
+    tiempo = obreros * 0.5
+
+    elemento_m = lista1[0]
+    elemento_d = lista1[1]
+
+    if a < elemento_m:
+        print("No tienes suficientes materiales")
+    if b < elemento_d:
+        print("No tienes suficientes dinero")
+    comprobardependencia(lista1,dependencias,listadeificios)
+
+    listadeificios.append[]
+    print(f"Construido tu {lista1} con un tiempo de {tiempo}")
+
+
+def comprobardependencia(estructura, dependencias,listadeificios = []):
+     if not estructura in dependencias:
+          return False
+     if dependencias[estructura] == [""]:
+          return False
+     if dependencias[estructura]in listadeificios:
+          continue
+     else:
+          print("No esta construido")
+
+     
